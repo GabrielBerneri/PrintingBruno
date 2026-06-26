8609d71ba708e6d52aace57a0953347880e9a692
gberneri <gabyberneri.gb@gmail.com>
2026-06-26 16:43:55 -0300

    Sacar seccion Categorias del footer

--- partials/site-chrome.php
+++ partials/site-chrome.php
@@ -237,20 +237,7 @@ function pb_render_footer(): void
                     </ul>
                 </div>
 
-                <div>
-                    <h4 class="footer-heading">Categorías</h4>
-                    <ul class="footer-links">
-                        <?php foreach (pb_category_items() as $item): ?>
-                            <li>
-                                <a href="<?= htmlspecialchars($item['href'], ENT_QUOTES, 'UTF-8') ?>">
-                                    <?= htmlspecialchars($item['label'], ENT_QUOTES, 'UTF-8') ?>
-                                </a>
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
-                </div>
-
-                <div>
+<div>
                     <h4 class="footer-heading">Contacto</h4>
                     <ul class="footer-contact">
                         <li>
